1640c987e951b5011bcbababbbc9d72014138149
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2023-11-10 16:49:46 +0300

    Finalized vanilla JS functional

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,53 @@ for (let i = 0; i < links.length; i++) {
     }
 }
 
+let buttons = document.getElementsByClassName("inlay-button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
 
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+
+    [burger, name, phone].forEach(item => {
+        if (!item.value) {
+            item.parentElement.style.background = "red";
+            hasError = true;
+        } else {
+            item.parentElement.style.background = "";
+        }
+    });
+    [burger, name, phone].forEach(item => {
+        item.value = "";
+    });
+    if (!hasError) {
+        alert("Спасибо за заказ!");
+    }
+}
+
+let prices = document.getElementsByClassName("price-title");
+document.getElementById("change-currency").onclick = function (e) {
+    let currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$") {
+        newCurrency = "€";
+        coefficient = 1.07;
+    } else if (currentCurrency === "€") {
+        newCurrency = "RSD";
+        coefficient = 109.72;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+
+}
